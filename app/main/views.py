973f5e022159bde238a2f168dbfebec134c55ca3
Kevine Musoni <kevinenicky@gmail.com>
2018-11-19 01:22:23 +0300

    user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,38 @@
-from flask import render_template
+from flask import render_template,url_for,abort
 from . import main
 from app import create_app
+from ..models import User
+from .forms import UpdateProfile
+from .. import db
 
 @main.route('/')
 def index():
     title = 'Home - Welcome to Pitch App'
     return render_template('index.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
--- app/models.py
+++ app/models.py
@@ -1,13 +1,20 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
+from flask_login import UserMixin
 # user class
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(255))
+    
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
 
     @property
     def password(self):
@@ -24,6 +31,11 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
